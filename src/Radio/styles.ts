80af482f2940e552d03eab91fbc3e4d3a5fcc4eb
Anna Yarossi <anna@cretia.dev>
2023-09-01 13:36:23 -0600

    Fix radio

--- src/Radio/styles.ts
+++ src/Radio/styles.ts
@@ -4,11 +4,12 @@ export const Label = styled.label`
 	display: flex;
 	flex-direction: row-reverse;
 	justify-content: flex-end;
-	&:focus:not([readonly]) {
-		border-color: var(--human-color--blue);
-	}
 
 	input {
 		margin-right: 4px;
+	}
+
+	&:focus:not([readonly]) {
+		border-color: var(--human-color--blue);
 	}
 `
